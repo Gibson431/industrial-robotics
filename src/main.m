13d9941e134a2f1bebd613564f0ed1a0494fd2d8
Ruby Kasif <ruby.kasif@student.uts.edu.au>
2023-09-28 10:54:07 +1000

    scaled substrate

--- src/main.m
+++ src/main.m
@@ -9,5 +9,11 @@ if ~useROS
     elle = Elle(transl(X_BASE_DISTANCE/2,0,0));
 end
 
+PlaceObject("Grow_Flat_Deep.ply",[0,0,0]);
+PlaceObject("Grow_Flat.ply",[0,0,0]);
+PlaceObject("Net_Pot.ply",[1,0,0]);
+PlaceObject("30mm_Substrate.ply",[1,0,0]);
+
+
 
 
