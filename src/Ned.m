527a67e6882eb99861aa4299a2201fd8d5c07917
Ruby Kasif <ruby.kasif@student.uts.edu.au>
2023-10-19 15:57:24 +1100

    aiashdg

--- src/Ned.m
+++ src/Ned.m
@@ -1,7 +1,11 @@
 classdef Ned < omronTM5
     methods
         function self = Ned(tr)
-            self.model.base = tr;
+            baseTr = eye(4);
+            if nargin == 1
+                baseTr = tr;
+            end
+            self.model.base = baseTr;
             self.model.animate([0 pi/2 0 0 0 0]);
         end
     end
