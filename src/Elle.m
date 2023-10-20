1e32439983634386e85376d60003ec5e1f6ea9fd
millieralston <145735968+millieralston@users.noreply.github.com>
2023-10-20 16:51:33 +1100

    initial guesses for elle
    to fix with offsets

--- src/Elle.m
+++ src/Elle.m
@@ -14,77 +14,88 @@ classdef Elle < UR3
             end
             % self.robot = UR3(baseTr);
             self.model.animate([0 -pi/2 0 0 0 pi/2]);
-
+            
             %             self.netpot = RobotNetpots(self.netpotCount);
             %             self.model.teach();
-
+            
             stepElle(self);
         end
-
+        
         %% Moving ELLE
         function self =  stepElle(self)
             % INITIAL GUESSES
-            % initialGuess = [
-            %     0.3770   -0.8796    2.0106   -1.1310    1.8850    0.0000
-            %     0   -1.1310    .5133   -1.3823    1.6336         0
-            %     3.7699   -2.0106   -2.5133   -1.7593   -1.0053         0
-            %     3.2673   -2.0106   -2.3876   -2.0106   -1.3823         0
-            %     ];
-
+            initialGuess = [
+                0.3770   -0.8796    2.0106   -1.1310    1.8850    0.0000
+                0   -1.1310    .5133   -1.3823    1.6336         0
+                3.7699   -2.0106   -2.5133   -1.7593   -1.0053         0
+                3.2673   -2.0106   -2.3876   -2.0106   -1.3823         0
+                ];
+            
             self.netpot = RobotNetpots(self.netpotCount);
             steps = length(self.netpot.netpotModel);
-
+            
             for i = 1 : steps
                 bTr = self.netpot.netpotModel{i}.base;
                 % self.netpot.netpotModel{1}.base;
-
+                
                 bx_pos = bTr.t(1);
                 by_pos = bTr.t(2);
                 bz_pos = bTr.t(3);
-
+                
                 waypoint1 = transl(bx_pos + 0.1,by_pos, bz_pos) * troty(-pi/2) * trotz(-pi);
                 waypoint2 = transl(bx_pos+0.02,by_pos,bz_pos) * troty(-pi/2) * trotz(-pi);
-
+                
                 if i <= 4
                     waypoint3 = transl(-0.2,-0.2,0.6) * troty(-pi/2);
                     waypoint4 = transl(-0.1,0.3-i*0.1,0.2) * troty(-pi/2);
-
                     waypoint = {waypoint1,waypoint2,waypoint3,waypoint4};
+                    
+                    guess = initialGuess(1,:);
+                    
                     wSteps = length(waypoint);
                 end
-
+                
                 if 4 < i
                     waypoint3 = transl(-0.2,-0.2,0.6) * troty(-pi/2);
                     waypoint4 = transl(-0.15,0.3-(i-4)*0.05,0.1)* troty(-pi/2);
                     waypoint = {waypoint1,waypoint2,waypoint3,waypoint4};
-
+                    
+                    guess = initialGuess(2,:);
+                    
                     wSteps = length(waypoint);
-
+                    
                 end
-
+                
                 if 8 < i
                     waypoint3 = transl(-0.2,-0.2,0.6) * troty(-pi/2);
                     waypoint4 = transl(0.2,0.3-(i-8)*0.05,0.1)* troty(-pi/2);
                     waypoint = {waypoint1,waypoint2,waypoint3,waypoint4};
-
+                    
+                    guess = initialGuess(3,:);
+                    
                     wSteps = length(waypoint);
-
+                    
                 end
-
+                
                 if 12 < i
                     waypoint3 = transl(-0.2,-0.2,0.6) * troty(-pi/2);
                     waypoint4 = transl(0.25,0.3-(i-12)*0.05,0.1)* troty(-pi/2);
                     waypoint = {waypoint1,waypoint2,waypoint3,waypoint4};
-
+                    
+                    guess = initialGuess(4,:);
+                    
                     wSteps = length(waypoint);
-
+                    
                 end
-
+                
                 for j = 1:wSteps
-                    nextJointState = self.model.ikcon(waypoint{j});
+                    if j==1
+                        nextJointState = self.model.ikcon(waypoint{j}, guess);
+                    else
+                        nextJointState = self.model.ikcon(waypoint{j});                     
+                    end
                     if j<=2
                         % nextJointState = self.model.ikine(waypoint{j},'mask',[1 1 1 1 1 0]);
-
                         self.moveElle(nextJointState);
                     end
                     if 2<j
@@ -99,7 +110,7 @@ classdef Elle < UR3
                 end
             end
         end
-
+        
         function self =  moveElle(self,nextJointState)
             currentJointState = self.model.getpos;
             steps = 20;
@@ -117,16 +128,16 @@ classdef Elle < UR3
             %      qMatrix(l+1,:) =  qMatrix(l,:) + deltaT*qdot';
             %  end
             qMatrix = jtraj(currentJointState,nextJointState,steps)
-
+            
             for i = 1:steps
                 % self.netpot.netpotModel{i}.base = self.model.fkine(qMatrix(i,:)) * SE3(troty(pi/2));
-
+                
                 self.model.animate(qMatrix(i,:));
                 drawnow();
                 pause(0.1);
             end
         end
-
+        
         function self =  moveElleNetpot(self,steps,i,nextJointState)
             currentJointState = self.model.getpos;
             % s = lspb(0,1,steps);
@@ -147,10 +158,10 @@ classdef Elle < UR3
             % Animate gripper and brick with end-effector
             for j = 1:steps
                 self.netpot.netpotModel{i}.base = self.model.fkine(qMatrix(j,:)) * SE3(troty(pi/2)) *SE3(trotx(-pi/2));
-
+                
                 self.model.animate(qMatrix(j,:));
                 self.netpot.netpotModel{i}.animate(0);
-
+                
                 drawnow();
                 pause(0.1);
             end
