c871d3821eed40a79d7fec43d34ff0a77757ab0c
Ruby Kasif <ruby.kasif@student.uts.edu.au>
2023-10-20 12:19:57 +1100

    weird things

--- src/Ned.m
+++ src/Ned.m
@@ -1,7 +1,11 @@
 classdef Ned < omronTM5
     methods
         function self = Ned(tr)
-            self.model.base = tr;
+             baseTr = transl(0,0,0);
+            if nargin ~= 0
+                baseTr = tr;
+            end
+            self.model.base = baseTr;
             self.model.animate([0 pi/2 0 0 0 0]);
         end
     end
