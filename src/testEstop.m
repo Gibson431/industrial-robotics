e375db7c5ee13f6cbedbc821599bf7149eb0f6bf
millieralston <145735968+millieralston@users.noreply.github.com>
2023-11-01 12:36:09 +1100

    button works and input reading: need to figure out why pausing wont return to release state when button is released

--- src/testEstop.m
+++ src/testEstop.m
@@ -2,27 +2,36 @@ close all
 clear
 clc
 hold on
-% Create an instance of the Elle class (assuming you have Elle class defined)
-robot = Elle(eye(4));
-% robot.model.teach();
+
+% robot = Elle(eye(4));
+a = arduino
+
+
 while true
-    robot.doStep();
-end
+    buttonPin = readDigitalPin(a, 'D2') % Change value to whichever digital pin is meant to be read.
 
-led_pin = 'D8'; % Change value to whichever digital pin is meant to be read.
-button1_pin = 'D2'; % Change value to whichever digital pin is meant to be read.
-button2_pin = 'D3'; % Change value to whichever digital pin is meant to be read.
-
-% Set the StopButton1 to false initially
-
-% Create a loop to continuously check the button state
-while true
-    buttonState = readDigitalPin(f, button1_pin);
-    if buttonState == 1
-        app.StopButton1 = true; % Set StopButton1 to true if button1 is pressed
-    end
-    
-    if app.StopButton1
-        break; % Exit the loop if button1 is pressed
+    if buttonPin == 1
+        % Code to run when buttonPin is equal to 1
+        disp('Button is not pressed. Running code...');
+        % Replace with your code to be executed
+    else if buttonPin == 0
+        % Code to pause when buttonPin is equal to 0
+        disp('Button is pressed. Pausing...');
+        % Add any necessary pause or delay here
+        pause(1);  
+        
     end
 end
+
+% if buttonPin == 1
+%     
+% else if buttonPin == 0
+%         disp('huh');
+%     end
+% end
+% 
+%     
+%     
+%     
+%     
+%     
