c96bf32d96f2e6b55ff2bcb0919ceec0fe12d86f
millieralston <145735968+millieralston@users.noreply.github.com>
2023-11-02 22:30:43 +1100

    ignore

--- src/eStop.m
+++ src/eStop.m
@@ -1,28 +1,7 @@
-
-% Create an instance of the Elle class (assuming you have Elle class defined)
-robot = Elle(eye(4));
-% robot.model.teach();
-while true
-    robot.doStep();
-end
-
-button1_pin = 'D2'; 
-% button2_pin = 'D3'; 
 % robot = Elle(eye(4));
 a = arduino
 
-% Set the StopButton1 to false initially
-
-% Create a loop to continuously check the button state
-while true
-    buttonState = readDigitalPin(f, button1_pin);
-    if buttonState == 1
-        app.StopButton1 = true; % Set StopButton1 to true if button1 is pressed
-    end
-    
-    if app.StopButton1
-        break; % Exit the loop if button1 is pressed
-    buttonPin = readDigitalPin(a, 'D2') % Change value to whichever digital pin is meant to be read.
+buttonPin = readDigitalPin(a, 'D2') % Change value to whichever digital pin is meant to be read.
 
     if buttonPin == 1
         % Code to run when buttonPin is equal to 1
@@ -32,7 +11,7 @@ while true
         % Code to pause when buttonPin is equal to 0
         disp('Button is pressed. Pausing...');
         % Add any necessary pause or delay here
-        break;  
+        break;
         
     end
 end
@@ -43,44 +22,3 @@ end
 %         disp('huh');
 %     end
 % end
-% 
- 
-%     
-% classdef eStop < handle
-%     properties
-%         isRunning = true;
-%         arduino;
-%         
-%     end
-%     
-%     methods
-%         function obj = testEstop()
-%             a = arduino();  % Initialize the Arduino object
-%             stopBut = readDigitalPin(a, 'D2');
-%             resBut = readDigitalPin(a, 'D3');
-%         end
-%         
-%         function Loop(obj)
-%             while true
-%                                 
-%                 if isRunning && stopBut == 1
-%                     disp('Stop button is pressed. Stopping the script...');
-%                     
-%                 end
-%                 
-%                 if isRunning  && stopBut == 0
-%                     disp('Stop button is pressed. Stopping the script...');
-%                     break;
-%                 end
-%                 
-%                 if resBut == 1
-%                     obj.isRunning = true;
-%                     disp('Resume button is pressed. Resuming the script...');
-%                 end
-% 
-%             end
-%         end
-%        
-%     end
-% end
-
